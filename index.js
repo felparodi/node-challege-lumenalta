48b075cdb525cf50fd4bf33d8bb3ba521d31ea61
Felipe Parodi <felipe.parodi91@gmail.com>
2024-04-02 17:30:57 -0300

    Solution

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
+
+
 const pooledDownload = (connect, save, downloadList, maxConcurrency) => {
-  return connect().then((connection) => {
-    const { download, close } = connection
-    return download(downloadList[0]).then((result) => save(result)) // download the first file and save the result
-  })
+  //console.log({downloadList: downloadList.length, maxConcurrency})
+  let pendingDownload = [...downloadList]
+  const connectPool = []
+  const closeConnectionList = [];
+    for(let i = 0; i  < downloadList.length && i < maxConcurrency; i++) {
+      //console.log({ 'startI': i })
+      const connectPoolItem = connect()
+        .catch((e) => {  
+          //console.log(e)
+          throw new Error('connection failed') 
+        })
+        .then(async (connection) => {
+          //console.log('startPool '+i)
+          const { download, close } = connection
+          closeConnectionList.push(close)
+          while(pendingDownload.length > 0) {
+            //console.log({ 'pool': i, 'pendingDownload': pendingDownload.length})
+            await download(pendingDownload.pop())
+              .then((result) => save(result))
+              .catch((e) => { 
+                throw e
+              })
+            //console.log( 'pool '+ i + ' end')
+          }
+          close()
+      })
+      connectPool.push(connectPoolItem);
+      //console.log({ 'connectPoolLength': connectPool.length })
+    }    
+  //console.log({connectPool: connectPool.length})
+  return Promise.all(connectPool)
+    .catch((e) => {
+      try {
+        closeConnectionList.forEach(c => c())
+      } catch {}
+      throw e
+    })
 }
 
 module.exports = pooledDownload
